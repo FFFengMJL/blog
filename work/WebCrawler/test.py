45042b5993600c6c5df925473b27a69cfe2b179d
fffengmjl <m781131011@outlook.com>
2019-04-16 14:27:20 +0800

    添加了笔记

--- work/WebCrawler/test.py
+++ work/WebCrawler/test.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+# import requests
 import requests
-import urllib
 from bs4 import BeautifulSoup
-url = "http://sdcs.sysu.edu.cn/news/notice"
+url = "http://sdcs.sysu.edu.cn/news"
 html_doc = requests.get(url)
+ck = html_doc.cookies # save the cookies
+print(ck)
+html_doc = requests.get(url, cookies=ck)
+print(vars(html_doc.request))
 
-html_doc = urllib2.Request(url)
 #模拟Mozilla浏览器进行爬虫
-html_docs.add_header("user-agent","Mozilla/5.0")
-html_doc = urllib2.urlopen(request)
+# request.add_header("user-agent","Mozilla/5.0")
+# html_doc = urllib.request.urlopen(request)
 # print(html_doc)
 
 #创建一个BeautifulSoup解析对象
 soup = BeautifulSoup(html_doc.text,"html.parser")
-print(html_doc.text)
+#print(html_doc.text)
 #获取所有的链接
 links = soup.find_all('a')
 print("\n所有的链接")
