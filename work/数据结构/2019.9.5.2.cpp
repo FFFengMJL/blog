9dc0afd95203f5a8a97961b65ba106b8d6fdaa94
fffengmjl <m781131011@outlook.com>
2019-09-06 11:42:26 +0800

    读取矩阵，有问题

---
+++ work/数据结构/2019.9.5.2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int num = 0, n = 0, data, sum = 0;
+    ifstream file;
+    file.open("test.txt", ios::in);
+    file >> num;
+    if (n >= 10) {
+        cout << "data error" << endl;
+        return 1;
+    }
+    for (int i = 0; i < num; i++) {
+        sum = 0;
+        for (int t = 0; t < num; t++) {
+            file >> data;
+            cout << data << '\t';
+            sum += data * data * data;
+        }
+        cout << "\t\t" << sum << endl;
+    }
+    file.close();
+    return 0;
+}
