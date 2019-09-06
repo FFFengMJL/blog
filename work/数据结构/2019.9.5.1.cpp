c9b28131e19228a0f8ef867889e61b921cee8397
fffengmjl <m781131011@outlook.com>
2019-09-06 11:15:51 +0800

    添加了数据结构实验课的例子

---
+++ work/数据结构/2019.9.5.1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int cale(int data)
+{
+    int sum = 0;
+    int bit;
+    while (data != 0) {
+        bit = data % 10; // 取余
+        data /= 10; // 自递除
+        sum += bit * bit * bit;
+    }
+    return sum;
+}
+
+int main()
+{
+    int data;
+    while (1) {
+        cin >> data;
+        if (data == 0) break;
+        if(data < 0) cout << "Not a true data, please input again" << endl;
+        else cout << cale(data) << endl;
+    }
+    return 0;
+}
