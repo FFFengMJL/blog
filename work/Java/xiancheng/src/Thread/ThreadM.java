55ed29d94e4c25baa85cf023aedbf8682c7eda56
fffengmjl <m781131011@outlook.com>
2019-11-07 17:35:08 +0800

    000

--- work/Java/xiancheng/src/Thread/ThreadM.java
+++ work/Java/xiancheng/src/Thread/ThreadM.java
@@ -3,7 +3,7 @@ package Thread;
 class ThreadM extends Thread {
     public void run() {
         try {
-              for (int i = 0; i < 3; i++) {
+              for (int i = 0; i < 5; i++) {
                     Thread.sleep(1000);
                     System.out.println("ThreadM");
               }
