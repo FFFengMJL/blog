e89f31d992f10292150bc5a869d1ce8bd37ade70
fffengmjl <m781131011@outlook.com>
2020-06-20 11:55:53 +0800

    feat: 添加了相关记录

--- work/test.cpp
+++ work/test.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    for (int i = 4; i <= 6; i++) {
-        for (int j = 1; j <= 4; j++) {
-            cout << "\"scene_evt170531_cp"<< i << "_q" << j << "_s10\":\"活动剧情/お子様ランチdeOHMYリュミエ~ル♪/scene_evt170531_cp" << i << "_q" << j << "_s10.csv\", ";
-        }
-    }
-
+int main()
+{
+    float a = 1.0 / 0.0;
+    float b = a;
+    cout << a << '\t' << b << endl;
+    printf("%d\n", a == b);
     return 0;
 }
